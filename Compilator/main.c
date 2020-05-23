3b03f9d5e799fea080e3e2cad0e9dd6a81bb0218
fmatheiu98 <matheiuflavius@gmail.com>
2020-05-23 10:24:02 +0300

    VM

--- Compilator/main.c
+++ Compilator/main.c
@@ -48,21 +48,21 @@ void err(const char* fmt, ...)
 void mvTest()
 {
 	Instr* L1;
-	int* v = allocGlobal(sizeof(long int));
+	int* v = allocGlobal(sizeof(int));
 	addInstrA(O_PUSHCT_A, v);
 	addInstrI(O_PUSHCT_I, 3);
-	addInstrI(O_STORE, sizeof(long int));
+	addInstrI(O_STORE, sizeof(int));
 	L1 = addInstrA(O_PUSHCT_A, v);
-	addInstrI(O_LOAD, sizeof(long int));
+	addInstrI(O_LOAD, sizeof(int));
 	addInstrA(O_CALLEXT, requireSymbol(&symbols, "put_i")->addr);
 	addInstrA(O_PUSHCT_A, v);
 	addInstrA(O_PUSHCT_A, v);
-	addInstrI(O_LOAD, sizeof(long int));
+	addInstrI(O_LOAD, sizeof(int));
 	addInstrI(O_PUSHCT_I, 1);
 	addInstr(O_SUB_I);
-	addInstrI(O_STORE, sizeof(long int));
+	addInstrI(O_STORE, sizeof(int));
 	addInstrA(O_PUSHCT_A, v);
-	addInstrI(O_LOAD, sizeof(long int));
+	addInstrI(O_LOAD, sizeof(int));
 	addInstrA(O_JT_I, L1);
 	addInstr(O_HALT);
 }
